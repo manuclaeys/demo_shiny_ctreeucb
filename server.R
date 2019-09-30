01e3e72ef0805181fbe8631ce1b6e4e33dad793c
manuclaeys <emmanuelle.claeys.pro@gmail.com>
2019-09-30 23:29:55 +0200

    interface  + test analyse

--- server.R
+++ server.R
@@ -7,6 +7,8 @@ library(dplyr)
 library(magrittr)
 library(lubridate)
 library(readr)
+library(bandit4abtest)
+library(partykit)
 #https://thedatalab.shinyapps.io/TrafficAccidents/
 
 options(shiny.maxRequestSize=10000000000*1024^2) #pour les data du grande taille
@@ -16,12 +18,27 @@ server <- shinyServer(function(input, output, session) {
 
   ##################### Importer la base ################
  
-  library(bandit4abtest)
-    dat <- abtest1
+  
+    dat <- abtest2
     
+    abtest_data <- dat
     
+    col_prediction <- function(){
+      data_ab <- abtest_data
+      data_ab$variation <- NA
+      data_ab[  is.na(data_ab$A) , "variation"   ] <- "B"
+      data_ab[  is.na(data_ab$B) , "variation"   ] <- "A"
+      data_ab[  is.na(data_ab$A) , "A"   ] <- 0
+      data_ab[  is.na(data_ab$B) , "B"   ] <- 0
+      data_ab[ , "variation"] <- as.factor(data_ab[ , "variation"] )
+      data_ab  <-   data_ab[ is.na(data_ab$variation) ==FALSE,] 
+      data_ab$action <- as.factor(as.integer(as.character(data_ab$A)) + as.integer(as.character(data_ab$B)))
+      return(data_ab)
+    }
+    
+
  
-  output$cont <- renderDataTable({
+    output$cont <- renderDataTable({
     
     head(dat, n = input$obs)
   })
@@ -39,7 +56,7 @@ server <- shinyServer(function(input, output, session) {
   ####Cartography###
   output$mapVars = renderUI(
     awesomeCheckboxGroup(inputId = "mapVars", 
-                         label = "Choose type of investigation", choices =  c("A", "B"), selected = "A", 
+                         label = "Choose type of investigation", choices =  c("1", "0"), selected = "1", 
                          inline = TRUE, status = "warning")
   )
   
@@ -67,19 +84,34 @@ server <- shinyServer(function(input, output, session) {
   ############### modifier notre base selon "type of investigation" ###############
   miniBase<-reactive({
    
-   # d<- subset(dat,dat[,Investigation.Type] %in% c(input$mapVars))
-    d <- dat %>% filter(Investigation.Type == input$mapVars)
+    myabtest_data   <-  col_prediction()
+    myabtest_data  <-   myabtest_data  %>% filter(myabtest_data$action == input$mapVars)
+    
+   # d <- subset(dat,dat[,Investigation.Type] %in% c(input$mapVars))
+  #  d <- dat %>% filter(Investigation.Type == input$mapVars)
    
-    return(d)
+    return( myabtest_data)
     
   })
   
+  
+  ##############Map function##################
+  
+  output$mymap <- renderLeaflet({
+    #base  <-  col_prediction()
+    base<- miniBase()
+    leaflet(data = base) %>% addTiles() %>%
+    #  addMarkers(~longitude, ~latitude)
+      addMarkers(~longitude, ~latitude, popup = ~as.character(countryID), label = ~as.character(countryID))
+    leaflet(data = base) %>% addTiles() %>% addMarkers(
+      clusterOptions = markerClusterOptions())
+    
+  })
+
+  
   #############################data explorer#################
   
-  abtest_data <- dat
 
-  abtest_data$A <- as.factor(abtest_data$A)
-  abtest_data$B <- as.factor(abtest_data$B)
   
   # Filter data, returning a data frame
   getabtest  <- reactive({
@@ -105,50 +137,115 @@ server <- shinyServer(function(input, output, session) {
   })
   
   output$plot1 <- renderPlot({
+    
+    
     #myabtest_data   <- getabtest()
-    myabtest_data   <-  myabtest_data 
+    #abtest_data <- col_prediction()
+    
+    #abtest_data$A <- as.factor(abtest_data$A)
+    #abtest_data$B <- as.factor(abtest_data$B)
+    
+    
+    myabtest_data   <-  col_prediction()
     
     require(gridExtra)
+    library(wesanderson)
     
     pie1 <- ggplot(myabtest_data, aes(x = "", fill = factor(device))) + 
       geom_bar(width = 1) +
-      theme(axis.line = element_blank(), 
+      theme(
+        plot.margin = margin(2,.8,2,.8, "cm"),
+        axis.line = element_blank(), 
             plot.title = element_text(hjust=0.5)) + 
       labs(fill="class", 
            x=NULL, 
            y=NULL, 
            title="Pie Chart of device", 
            caption="Source: mpg") +
-      coord_polar(theta = "y", start=0)
+      coord_polar(theta = "y", start=0)+
+      scale_fill_brewer(palette = "Reds")
     
-    pie2 <- ggplot(myabtest_data, aes(x = "", fill = factor(name))) + 
+    pie2 <- ggplot(myabtest_data, aes(x = "", fill = factor(name)))  + 
       geom_bar(width = 1) +
-      theme(axis.line = element_blank(), 
+      theme(
+        plot.margin = margin(2,.8,2,.8, "cm"),
+        axis.line = element_blank(), 
             plot.title = element_text(hjust=0.5)) + 
       labs(fill="class", 
            x=NULL, 
            y=NULL, 
-           title="Pie Chart of O.S.", 
-           caption="Source: mpg") +
-      coord_polar(theta = "y", start=0)
+           title="Pie Chart of O.S.")+ 
+          # caption="Source: mpg") +
+      coord_polar(theta = "y", start=0) +
+      scale_fill_brewer(palette = "Blues")
+     # scale_fill_manual(values=wes_palette(n=length(levels(as.factor(myabtest_data$name))), name="GrandBudapest1"))
     
     
-    pie3 <- ggplot(myabtest_data, aes(x = "", fill = factor(userAgent))) + 
+    pie3 <- ggplot(myabtest_data, aes(x = "", fill = factor(userAgent)))  + 
       geom_bar(width = 1) +
-      theme(axis.line = element_blank(), 
+      theme(
+        plot.margin = margin(2,.8,2,.8, "cm"),
+        axis.line = element_blank(), 
             plot.title = element_text(hjust=0.5)) + 
       labs(fill="class", 
            x=NULL, 
            y=NULL, 
            title="Pie Chart of U.A.", 
            caption="Source: mpg") +
-      coord_polar(theta = "y", start=0)
+      coord_polar(theta = "y", start=0)+
+      scale_fill_brewer(palette = "Greens")
     
     
     grid.arrange(pie1, pie2, pie3, ncol=3)
     
     
   })
+  
+  output$plot2 <- renderPlot({
 
+    plot(x=1:10,y=1:10)
+    
+  })
+
+  
+  
+  
+  
+  output$plot4 <- renderPlot({
+    
+    library(bayesAB)
+    data_for_tree <- col_prediction()
+    x <- as.numeric(as.character(   data_for_tree[ data_for_tree$variation == "A" , "action"]))
+    y <- as.numeric(as.character( data_for_tree[ data_for_tree$variation == "B" , "action"]))
+    test1 <- bayesTest(x, y, distribution = "bernoulli", priors = c("alpha" = 10, "beta" = 10))
+    #    print(test1)
+     temp <- plot(test1)
+      temp$posteriors
+  #  plot(x=1:10,y=1:10)
+    
+  })
+  
+  
+  
+  
+  
+  
+  output$plot3 <- renderPlot({
+
+    # Variation #
+    data_for_tree <- col_prediction()
+    
+    my_formula <- as.formula("action ~ name + device + userAgent + variation")
+    
+    ### classification
+    my_tree  <- ctree(my_formula,data = data_for_tree)
+    
+    plot(my_tree  )
+    
+  })
+  
+  
+
+  
  
 })
--- ui.R
+++ ui.R
@@ -14,20 +14,26 @@ library(readr)
 shinyUI(tagList(
   dashboardPage(skin = "blue",
                 
-                dashboardHeader(title = "Aviation Accident"),
+                dashboardHeader(title = "AB test Dashboard"),
                 dashboardSidebar(width = 240, sidebarMenu(
                   
                   menuItem("Instructions", tabName = "genIns", icon = icon("info-circle")),
                   menuItem("Data", tabName = "uploadData", icon = icon("table")),
                   menuItem("Data Explorer", tabName = "explore", icon = icon("bar-chart-o")),
-                  menuItem("Map", tabName = "carte", icon = icon("fa fa-eercast"))
-                  
-                  
+                  menuItem("Map", tabName = "carte", icon = icon("fa fa-eercast")),
+                  menuItem("Dynamic Allocation ", tabName = "algo", icon = icon("fa fa-cog"))
+
                 )
               
                 ),
                 dashboardBody(
                   tabItems(
+                    
+
+                    
+                    
+                    
+                    
                     tabItem(tabName = "genIns",
                             fluidPage(
                               
@@ -50,12 +56,7 @@ shinyUI(tagList(
                               box(width = 15, status = "primary",background = "navy",
                                   h5("This Application is powered by XX", a("LinkedIn", href="https://www.linkedin.com/in/jehan-drissi-360409124/"))
                               )
-                              
-                              
-                              
-                              
-                              
-                            )
+                          )
                     ),
                     # First tab content
                     tabItem(tabName = "uploadData",
@@ -78,44 +79,38 @@ shinyUI(tagList(
                             fluidPage(
                               titlePanel("Aviation data explorer"),
                               fluidRow(
+                               
+                                
+                                
                                 column(3,
+                                       plotOutput("plot4"),
                                        wellPanel(
-                                         h4("Select options"),
-                                         h5("This apps allows us to analyze easily the aviation data set. Select the parameters
-                                            to filter the data."),
-                                         sliderInput("year", "Year of the accident", 1982, 2017, value = c(1982, 2017)),
-                                         sliderInput("fatalInjuries", "At least X deaths",
-                                                     0, 350, 0, step = 1),
-                                         #textInput("country", "Country name"),
-                                         uiOutput("Country"),
-                                         selectInput("aircraftDamage", "Damage of the aircraft",
-                                                     c("All", "Destroyed", "Substantial", "Minor")),
-                                         selectInput("aircraftCategory", "Category of the aircraft",
-                                                     c("All", "Airplane", "Balloon", "Blimp", "Glider", "Gyrocraft",
-                                                       "Gyroplane", "Helicopter", "Powered-Lift", "Powered Parachute",
-                                                       "Rocket", "Ultralight", "Unknown", "Weight-Shift")),
-                                         selectInput("reportStatus", "Status of the report",
-                                                     c("All", "Factual", "Foreign", "Preliminary", "Probable Cause")),
-                                         selectInput("purposeFlight", "Purpose of the flight",
-                                                     c("All", "Aerial Application", "Aerial Observation", "Air Drop", "Air Race/Show",
-                                                       "Banner Tow", "Business", "Executive/Corporate", "External Load", "Ferry",
-                                                       "Firefighting", "Flight Test", "Glider Tow", "Instructional",
-                                                       "Other Work Use", "Personal", "Positioning", "Public Aircraft",
-                                                       "Public Aircraft - Federal", "Public Aircraft - Local",
-                                                       "Public Aircraft - State", "Skydiving", "Unknown"))
-                                         )
+
+                                       )
                                 ),
+                                
+                                
                                 column(9,
-                                       plotOutput("plot1"),
+                                       plotOutput("plot3"),
                                        wellPanel(
                                          span("Number of flights selected:",
                                               textOutput("n_accident")
                                          )
                                        )
                                 )
-                            )
+                                
+                            ),
+                            fluidRow(
+                              #column(12 ,plotOutput("plot2"))
+                              column(9,
+                                     plotOutput("plot1"),
+                                     wellPanel(
+                                       p("Number of flights selected:"
+                                       )
+                                     )
                               )
-                            
+                            )
+                         )
                             
                     ),
                     tabItem(tabName = "carte",
@@ -124,7 +119,43 @@ shinyUI(tagList(
                             uiOutput("mapVars"),
                             h5("Use the Zoom for more details"),
                             leafletOutput("mymap")
+                    ),
+                    
+                    tabItem(tabName = "algo",
+                            fluidPage(
+                              
+                              titlePanel("todo"),
+                        #      br(),
+                              fluidRow(
+                                
+                                column(9,
+                                       
+                                       box(width = 7, status = "primary",background = "navy",
+                                           h4("Content"),
+                                           h5("The NTSB aviation accident database contains information from 1962 and later about civil aviation accidents and selected incidents within the United States, 
+                                 its territories and possessions, and in international waters."),
+                                           br(),
+                                           h4("Acknowledgements"),
+                                           h5("Generally, a preliminary report is available online within a few days of an accident. Factual information is added when available,
+                                 and when the investigation is completed, the preliminary report is replaced with a final description of the accident and its probable cause.
+                                 Full narrative descriptions may not be available for dates before 1993, cases under revision, or where NTSB did not have primary investigative
+                                 responsibility."),
+                                           br(),
+                                           h4("Source:", a("Kaggle", href="https://www.kaggle.com/khsamaha/aviation-accident-database-synopses/data"))) 
+                                       
+                                       ), 
+                                
+                                column(3,
+                                       plotOutput("plot2"),
+                                       wellPanel(
+                                         
+                                       )
+                                )
+                              )
+                            )
                     )
+                    
+                    
                 )
-                  )
+                )
 )))
